dc7a63a8c79305a84196b9d3ee7067e0aa49038b
Asbjørn Thegler <asbjoern@gmail.com>
2015-09-25 01:17:49 +0200

    renamed, added time and colors

--- sjak_colors.pl
+++ sjak_colors.pl
@@ -2,6 +2,7 @@
 use Modern::Perl;
 use Mojo::mysql;
 use File::Slurp;
+use DateTime;
 
 if (scalar @ARGV < 1) {
   say "Usage: ./lok.pl [lokNumber] [outputfile.geojson]";
@@ -16,8 +17,17 @@ say "Building GeoJSON about [lok $lok] to file [$outfile]";
 my $mysql = Mojo::mysql->new('mysql://root@/nathejk15');
 my $db = $mysql->db;
 
+my @colors = (
+  '#a6cee3', '#1f78b4', '#b2df8a', '#33a02c',
+  '#fb9a99', '#e31a1c', '#fdbf6f', '#ff7f00',
+  '#cab2d6', '#6a3d9a', '#ffff99', '#b15928'
+);
+
+my %color_mem = ();
+
 my $results = $db->query('
   SELECT
+    nathejk_checkIn.createdUts AS tidspunkt,
     nathejk_member.title AS bandit,
     nathejk_team.title AS sjak,
     location
@@ -36,15 +46,21 @@ foreach my $catch (@{$results}) {
   my $latitude = $geo[0];
   my $longitude = $geo[1];
 
+  # sometimes local map coordinates are used. ignoring those
   next unless defined $longitude;
-  # sometimes, local map coordinates are used. ignoring
+
+  my $color = select_color($catch->{sjak});
+  my $dt = DateTime->from_epoch({epoch => $catch->{tidspunkt}});
+  my $time = $dt->hms . ' ' . $dt->dmy;
 
   my $string =
      '{
         "type": "Feature",
         "properties": {
           "bandit": "' . $catch->{bandit} . '",
-          "sjak": "' . $catch->{sjak} . '"
+          "sjak": "' . $catch->{sjak} . '",
+          "tidspunkt": "' . $time . '",
+          "marker-color": "' . $color . '"
         },
         "geometry": {
           "type": "Point",
@@ -61,3 +77,11 @@ foreach my $catch (@{$results}) {
 my $geojson = '{"type": "FeatureCollection","features": [' . join(',', @features) . ']}';
 
 write_file($outfile, {binmode => ':utf8'}, $geojson);
+
+
+sub select_color {
+  my $sjak = shift;
+  return $color_mem{$sjak} if (exists $color_mem{$sjak});
+  $color_mem{$sjak} = pop(@colors);
+  return $color_mem{$sjak};
+}
